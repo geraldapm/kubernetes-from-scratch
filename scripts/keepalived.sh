950749807c9989c5490e1a49622be3a451194e73
Gerald Mahayadnya <geraldiaditya1111@gmail.com>
2025-12-05 22:53:38 +0700

    feat: changed from single CA to multi-intermediate CA

--- scripts/keepalived.sh
+++ scripts/keepalived.sh
@@ -1,11 +1,46 @@
 #!/bin/bash
-# Setup Keepalived for High Availability on Control Plane Nodes and listen for 6443 port. Find a way to get the primary network interface dynamically.
+# Setup Keepalived for High Availability on Control Plane Nodes and listen for 6443 port
 set -euxo pipefail
 
 sudo apt-get update
 sudo apt-get install -y keepalived
 
+BIND_PORT=6443
+
+cat <<EOF | sudo tee /etc/keepalived/check_apiserver.sh
+#!/bin/bash
+if ! (ip addr | grep -q '$VIP_ADDRESS/'); then
+  if grep -zo 'unicast_peer[[:space:]]*{[[:space:]]*}' /etc/keepalived/keepalived.conf; then
+    if arping -f -I $(ip route get $VIP_ADDRESS | cut -d' ' -f3 | head -1) -c 3 $VIP_ADDRESS; then
+      if curl -k https://$VIP_ADDRESS:$BIND_PORT; then
+        exit 1
+      fi
+    fi
+  fi
+fi
+
+PORTS=$(netstat -nltp)
+echo $PORTS | grep -q $BIND_PORT
+if [ $? -ne 0 ]; then
+  echo $(date): keepalived failed to find kube-apiserver bound to port >> /etc/keepalived/log
+  exit 1
+fi
+EOF
+
+cat <<EOF | sudo tee /etc/keepalived/keepalived_state.sh
+#!/bin/bash
+echo $(date): keepalived state: "$@" >> /etc/keepalived/log
+EOF
+
+chmod 755 /etc/keepalived/check_apiserver.sh
+chmod 755 /etc/keepalived/keepalived_state.sh
+
 cat <<EOF | sudo tee /etc/keepalived/keepalived.conf
+global_defs {
+  router_id LVS_DEVEL
+  enable_script_security
+}
+
 vrrp_instance VI_1 {
     state MASTER
     interface $(ip route | grep $SUBNET | awk '{print $3'} | head -n 1) 
@@ -23,11 +58,14 @@ vrrp_instance VI_1 {
     track_script {
         chk_kube_apiserver
     }
+    notify /etc/keepalived/keepalived_state.sh
 }  
 vrrp_script chk_kube_apiserver {
-    script "curl -s --insecure https://localhost:6443/healthz || exit 1"
-    interval 2
-    weight -20
+    script "/etc/keepalived/check_apiserver.sh"
+    interval 5
+    weight 0
+    fall 10
+    rise 2
 }
 EOF
 
